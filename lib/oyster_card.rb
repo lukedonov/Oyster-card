20b68691213e93613cca72af49a6c2511b2222a7
Luke Donovan <lukelamnisos@icloud.com>
2020-02-24 15:23:10 +0000

    added tap_out, top_up and check_balance tests and passes

--- lib/oyster_card.rb
+++ lib/oyster_card.rb
@@ -1,3 +1,27 @@
 class OysterCard
+  attr_reader :balance, :fare
+
+  def initialize
+    @balance = 50
+    @fare = fare
+  end
+
+  def check_balance
+    @balance
+  end
+
+  def top_up(num)
+    fail "Over top-up limit" if over_limit?(num)
+    @balance += num
+  end
+
+  def tap_out
+    @balance -= @fare
+  end
+
+  private
+  def over_limit?(num)
+    @balance + num > 50
+  end
 
 end
--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,5 +1,36 @@
 require 'oyster_card'
 
 describe OysterCard do
-    it { is_expected.to respond_to :check_balance }
+  it { is_expected.to respond_to :check_balance }
+  it { is_expected.to respond_to :tap_out }   
+  it { is_expected.to respond_to(:top_up).with(1).argument }
+  
+  describe "#check_balance" do
+    it "returns the card balance" do 
+      subject.instance_variable_set(:@balance, 20)
+      expect(subject.check_balance).to eq 20
+    end
+  end
+
+  describe "#top_up" do
+    it "adds money to the oyster card" do
+      subject.instance_variable_set(:@balance, 0)
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+
+    it "raises an error when you top up too much" do
+      subject.instance_variable_set(:@balance, 50)
+      expect { subject.top_up(2) }.to raise_error
+    end
+  end
+
+  describe "#tap_out" do 
+    it "deducts the fare from the users balance" do
+      subject.instance_variable_set(:@fare, 5)
+      subject.instance_variable_set(:@balance, 10)
+      subject.tap_out
+      expect(subject.balance).to eq 5
+    end
+  end
 end
